10e35d660a200ad4695f721051ea166798619bde
midoks <midoks@163.com>
2016-12-17 00:47:01 +0800

    update

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,30 @@
 #!/bin/sh
 
+echo -n "You PHP Dir?"
+read PHP_DIR
+# case $PHP_DIR in
+#     y|Y|yes|Yes)
+#         PHP_DIR='/usr/local/php'
+#         ;;
+#     n|N|no|No)
+#         exit 0
+#         ENV='uat'
+#         ;;
+# esac
+
 wget https://github.com/midoks/md_xhprof/archive/master.zip
+
+unzip master.zip 
+rm -f master.zip
+
+cd md_xhprof-master/src
+
+
+#/usr/local/php70
+
+$PHP_DIR/bin/phpize
+./configure \
+--with-php-config=$PHP_DIR/bin/php-config
+make && make install
+
+rm -rf md_xhprof-master
